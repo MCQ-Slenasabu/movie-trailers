ccfcd052f8fe2845225cd50c3015d41155aa8eda
Donny Bottse <dbottse@mirabeau.nl>
2016-10-21 00:17:03 +0200

    remove movie-trailers.Business project

--- movie-trailers.Business/Interfaces/IMovie.cs
+++
@@ -1,9 +0,0 @@
-﻿using movie_trailers.Models;
-
-namespace movie_trailers.Interfaces
-{
-    interface IMovie
-    {
-        Movie GetMovieInformation(string search);
-    }
-}
--- movie-trailers.Business/Interfaces/IVideo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace movie_trailers.Interfaces
-{
-    interface IVideo
-    {
-        string GetVideoUrl(string search, bool embbed);
-    }
-}
